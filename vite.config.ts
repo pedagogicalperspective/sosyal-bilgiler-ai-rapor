5dd3f2970ab58748a6cfe166ededa3cc33436aec
pedagogicalperspective <pedagogicalperspective@gmail.com>
2026-03-19 15:28:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({'/sosyal-bilgiler-ai-rapor/'
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
